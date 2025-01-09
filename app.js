817d7bf8cec1c35c37335e2d55bd4a912a274c46
Celine Himbaza <celine.himbaza@gmail.com>
2025-01-09 16:39:28 +0200

    Vue Component

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ data(){
     return{
         
 showbook:true,
-tittle:'Change your thinking Change your life',
-author:'Brain Tracy',
-age:50
+// tittle:'Change your thinking Change your life',
+// author:'Brain Tracy',
+// age:
 
+//v for/array
+books:[
+    {tittle:'Change your thinking Change your life',author:'Brian Tracy',age:45, img: 'assets/1.jpg'},
+    {tittle:'The choice',author:'John',age:50, img: 'assets/2.jpg'},
+    {tittle:'The pathway to destiny',author:'Brian Tracy',age:45, img: 'assets/3.jpg'}
+]
     }
 },
 methods: {
