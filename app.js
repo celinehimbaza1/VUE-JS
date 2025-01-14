7a92741662795bf566e5bed5b8c57038a16ae6e3
Celine Himbaza <celine.himbaza@gmail.com>
2025-01-14 13:48:21 +0200

    Creating Vue app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ showbook:true,
 
 //v for/array
 books:[
-    {tittle:'Change your thinking Change your life',author:'Brian Tracy',age:45, img: 'assets/1.jpg'},
+    {tittle:'Change your thinking',author:'Brian Tracy',age:45, img: 'assets/1.jpg'},
     {tittle:'The choice',author:'John',age:50, img: 'assets/2.jpg'},
     {tittle:'The pathway to destiny',author:'Brian Tracy',age:45, img: 'assets/3.jpg'}
 ]
